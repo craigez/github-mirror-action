ede8a220358f7e82b8fdec7c30223b52a050a475
Craig Northway <quic_craign@quicinc.com>
2024-09-03 11:43:25 +1000

    Test customized action
    Signed-off-by: Craig Northway <quic_craign@quicinc.com>

--- action.sh
+++ action.sh
@@ -11,4 +11,9 @@ git config --global --add safe.directory /github/workspace
 git clone --bare "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" . || exit 1
 git remote add --mirror=fetch mirror "${REMOTE}" || exit 1
 git fetch mirror +refs/heads/*:refs/remotes/origin/* || exit 1
-git push --force --mirror --prune origin || exit 1
+
+if [-z "${TARGET_REFSPACE}"]; then
+  git push --force --mirror --prune origin "${TARGET_REFSPACE}" || exit 1
+else
+  git push --force --mirror --prune origin || exit 1
+fi
